31d8f7622f05a09fcd0a3f375e5de22ca260beb1
Delta200513 <151935568+Delta200513@users.noreply.github.com>
2024-04-19 14:52:32 +0300

    Update hardware_tasks.sql

--- hardware_tasks.sql
+++ hardware_tasks.sql
@@ -15,13 +15,13 @@ FROM hardware
 WHERE title LIKE '%Клавиатура%'
 ORDER BY price ASC;
 -- 5.
-SELECT COUNT() AS totalitems, tag
+SELECT COUNT(*) AS totalitems, tag
 FROM hardware
 WHERE tag IS NOT NULL
 GROUP BY tag
 ORDER BY totalitems DESC;
 -- 6.
-SELECT COUNT() AS discount
+SELECT COUNT(*) AS discount
 FROM hardware
 WHERE tag = 'discount';
 -- 7.
@@ -34,4 +34,4 @@ LIMIT 1;
 INSERT INTO hardware (title, price, amount, tag)
 VALUES ('Ноутбук Lenovo 2BXKQ7E9XD', 54500, 1, 'new');
 -- 9.
-DELETE FROM hardware WHERE title = ' 18 ';
+DELETE FROM hardware WHERE id = '18';
